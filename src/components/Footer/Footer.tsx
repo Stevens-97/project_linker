ec8a94a0b81aefe7e1f803bdaaae2c87f4249570
Stevens-97 <86322472+Stevens-97@users.noreply.github.com>
2022-04-02 14:10:09 +0100

    Added responsive units to navbar and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,9 @@ import styles from "./footer.module.css"
 export default function Footer() {
   return (
     <div className={styles.footerSection}>
+      <div className={styles.logoSection}>
         <img src="https://i2.lensdump.com/i/rxOZQP.png" alt='project linker logo' className={styles.logo}></img>
+        </div>
         <div className={styles.copyRight}>
             <p >Copyright 2022 ©</p>
         </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,9 @@ import styles from "./navbar.module.css"
 export default function Navbar() {
   return (
     <div className={styles.navbarSection}>
+      <div className={styles.logoSection}>
     <img src="https://i2.lensdump.com/i/rxOZQP.png" alt='project linker logo' className={styles.logo}></img>
+    </div>
     <div className={styles.menu}>
         <button className={styles.loginButton}>Sign in</button>
     </div>
